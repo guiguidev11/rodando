a85eeec86656fa62f03edcd9f66ed5e50e5cb054
Guilherme desktop <guilherrmetwister@gmail.com>
2023-11-11 22:46:06 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function toggleMode(){
   const html = document.documentElement
-
-  // if(html.classList.contains('light')){
-  //   html.classList.remove('light')
-  // } else{
-  //   html.classList.add('light')
-  // }
   html.classList.toggle('light')
+
+  const img = document.querySelector("#profile img")
+
+  if(html.classList.contains('light')){
+    img.setAttribute('src', './assets/assets/avatar-light.png')
+  } else{
+    img.setAttribute("src", "./assets/assets/avatar.png")
+  }
+  // pegar a tag img
+  
+  // substituir a imagem
+  // se tiver light mode adicionar a imagem light
+  // se tiver sem light mode, manter a imagem normal
 }
+
+
